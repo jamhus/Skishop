2e98c403a1fb43bef1bd52d230f1188ee7ad8645
Jamal Hussein <jamal.hussein@aditro.com>
2022-03-03 16:40:45 +0100

    Last commit

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -82,6 +82,7 @@ namespace API.Controllers
                 ShippingAddress = orderDto.ShippingAddress,
                 SubTotal = subtotal,
                 DeliveryFee = deliveryFee,
+                PaymentIntentId = basket.PaymentIntentId,
             };
 
             _context.Orders.Add(order);
